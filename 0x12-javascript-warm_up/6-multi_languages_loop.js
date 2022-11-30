26ff63a07abf1214f54d090af5154fded0320ffa
Uriel-kas <urielkas.uk@gmail.com>
2022-11-30 03:56:26 -0800

    Loop

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -1,6 +1,6 @@
 #!/usr/bin/node
 
 const multiLangs = ['C is fun', 'Python is cool', 'JavaScript is amazing'];
-multiLangs.forEach((element) => {
+multiLangs.forEach(element) => {
 	console.log(element);
-});
+}
